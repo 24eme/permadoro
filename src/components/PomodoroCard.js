2e6fdb59d2349b4c771bd76bc2b5357a194594c5
Vincent LAURENT <vince.laurent@gmail.com>
2025-11-19 10:29:52 +0100

    Notifications

--- src/components/PomodoroCard.js
+++ src/components/PomodoroCard.js
@@ -4,9 +4,9 @@ const shortbreakMinutes = 5;
 const longbreakMinutes = 15;
 const totalMinutes = 130;
 const pomodoroPeriods = {
-  pomodoro: { title: "🍅 Pomodoro", duration: pomodoroMinutes * 1000 * 60, color: '#dc3545'},
-  shortbreak: { title: "☕ Pause Courte", duration: shortbreakMinutes * 1000 * 60, color: '#ffc107'},
-  longbreak: { title: "🏖️ Pause longue", duration: longbreakMinutes * 1000 * 60, color: '#ffc107'}
+  pomodoro: { title: "🍅 Pomodoro", notification: "On ce concentre sur sa tâche pendant 25 minutes, aucune distraction !", duration: pomodoroMinutes * 1000 * 60, color: '#dc3545'},
+  shortbreak: { title: "☕ Pause Courte", notification: "On lève les mains du clavier, courte pause de 5 minutes !", duration: shortbreakMinutes * 1000 * 60, color: '#ffc107'},
+  longbreak: { title: "🏖️ Pause longue", notification: "Loooooonnnngue pause, 15 minutes pour chiller !", duration: longbreakMinutes * 1000 * 60, color: '#ffc107'}
 }
 
 export default {
@@ -14,23 +14,24 @@ export default {
   props: ["index", "icon", "isactive", "partof"],
   data() {
     return {
-        title: pomodoroPeriods.pomodoro.title,
+        title: pomodoroPeriods.pomodoro.title, notification: pomodoroPeriods.pomodoro.notification,
         active: false,
         minuteRest: null,
         secondRest: null,
         startDateHistoric: null,
         startDate: new Date(),
+        hasNotification: Notification.permission === "granted",
         duration: 0,
         color: pomodoroPeriods.pomodoro.color,
         periods: [
-          { startTime: 0, duration: pomodoroPeriods.pomodoro.duration, title: pomodoroPeriods.pomodoro.title, color: pomodoroPeriods.pomodoro.color, width: Math.round(pomodoroMinutes / totalMinutes * 100) + '%' },
-          { startTime: 25 * 1000 * 60, duration: pomodoroPeriods.shortbreak.duration, title: pomodoroPeriods.shortbreak.title, color: pomodoroPeriods.shortbreak.color, width: Math.round(shortbreakMinutes / totalMinutes * 100) + '%' },
-          { startTime: 30 * 1000 * 60, duration: pomodoroPeriods.pomodoro.duration, title: pomodoroPeriods.pomodoro.title, color: pomodoroPeriods.pomodoro.color, width: Math.round(pomodoroMinutes / totalMinutes * 100) + '%' },
-          { startTime: 55 * 1000 * 60, duration: pomodoroPeriods.shortbreak.duration, title: pomodoroPeriods.shortbreak.title, color: pomodoroPeriods.shortbreak.color, width: Math.round(shortbreakMinutes / totalMinutes * 100) + '%' },
-          { startTime: 60 * 1000 * 60, duration: pomodoroPeriods.pomodoro.duration, title: pomodoroPeriods.pomodoro.title, color: pomodoroPeriods.pomodoro.color, width: Math.round(pomodoroMinutes / totalMinutes * 100) + '%' },
-          { startTime: 85 * 1000 * 60, duration: pomodoroPeriods.shortbreak.duration, title: pomodoroPeriods.shortbreak.title, color: pomodoroPeriods.shortbreak.color, width: Math.round(shortbreakMinutes / totalMinutes * 100) + '%' },
-          { startTime: 90 * 1000 * 60, duration: pomodoroPeriods.pomodoro.duration, title: pomodoroPeriods.pomodoro.title, color: pomodoroPeriods.pomodoro.color, width: Math.round(pomodoroMinutes / totalMinutes * 100) + '%' },
-          { startTime: 115 * 1000 * 60, duration: pomodoroPeriods.longbreak.duration, title: pomodoroPeriods.longbreak.title, color: pomodoroPeriods.longbreak.color, width: Math.round(longbreakMinutes / totalMinutes * 100) + '%' }
+          { startTime: 0, duration: pomodoroPeriods.pomodoro.duration, title: pomodoroPeriods.pomodoro.title, notification: pomodoroPeriods.pomodoro.notification, color: pomodoroPeriods.pomodoro.color, width: Math.round(pomodoroMinutes / totalMinutes * 100) + '%' },
+          { startTime: 25 * 1000 * 60, duration: pomodoroPeriods.shortbreak.duration, title: pomodoroPeriods.shortbreak.title, notification: pomodoroPeriods.shortbreak.notification, color: pomodoroPeriods.shortbreak.color, width: Math.round(shortbreakMinutes / totalMinutes * 100) + '%' },
+          { startTime: 30 * 1000 * 60, duration: pomodoroPeriods.pomodoro.duration, title: pomodoroPeriods.pomodoro.title, notification: pomodoroPeriods.pomodoro.notification, color: pomodoroPeriods.pomodoro.color, width: Math.round(pomodoroMinutes / totalMinutes * 100) + '%' },
+          { startTime: 55 * 1000 * 60, duration: pomodoroPeriods.shortbreak.duration, title: pomodoroPeriods.shortbreak.title, notification: pomodoroPeriods.shortbreak.notification, color: pomodoroPeriods.shortbreak.color, width: Math.round(shortbreakMinutes / totalMinutes * 100) + '%' },
+          { startTime: 60 * 1000 * 60, duration: pomodoroPeriods.pomodoro.duration, title: pomodoroPeriods.pomodoro.title, notification: pomodoroPeriods.pomodoro.notification, color: pomodoroPeriods.pomodoro.color, width: Math.round(pomodoroMinutes / totalMinutes * 100) + '%' },
+          { startTime: 85 * 1000 * 60, duration: pomodoroPeriods.shortbreak.duration, title: pomodoroPeriods.shortbreak.title, notification: pomodoroPeriods.shortbreak.notification, color: pomodoroPeriods.shortbreak.color, width: Math.round(shortbreakMinutes / totalMinutes * 100) + '%' },
+          { startTime: 90 * 1000 * 60, duration: pomodoroPeriods.pomodoro.duration, title: pomodoroPeriods.pomodoro.title, notification: pomodoroPeriods.pomodoro.notification, color: pomodoroPeriods.pomodoro.color, width: Math.round(pomodoroMinutes / totalMinutes * 100) + '%' },
+          { startTime: 115 * 1000 * 60, duration: pomodoroPeriods.longbreak.duration, title: pomodoroPeriods.longbreak.title, notification: pomodoroPeriods.longbreak.notification, color: pomodoroPeriods.longbreak.color, width: Math.round(longbreakMinutes / totalMinutes * 100) + '%' }
         ],
     }
   },
@@ -49,6 +50,10 @@ export default {
       this.startDate = new Date(this.startDateHistoric.getTime() + (Math.floor((today.getTime() - this.startDateHistoric.getTime()) / (7800 * 1000)) * 7800 * 1000))
     },
     updateTimer() {
+      let lastStartTimePeriod = null
+      if(this.period) {
+        lastStartTimePeriod = this.period.startTime
+      }
       let now = new Date();
       let duration = now.getTime() - this.startDate.getTime();
       for(let period of this.periods) {
@@ -66,20 +71,33 @@ export default {
         this.title = this.period.title
         this.color = this.period.color
       }
+      if(lastStartTimePeriod && lastStartTimePeriod != this.period.startTime) {
+        this.changePeriod(this.period)
+      }
       if(this.duration >= totalMinutes * 1000 * 60) {
         this.updateStartDate();
         this.updateTimer()
         this.$emit('end', this);
       }
     },
+    changePeriod(newPeriod) {
+      if(!this.active || !this.activeNotifications) {
+        return;
+      }
+      new Notification(this.icon + ' ' + newPeriod.title, {
+        body: newPeriod.notification
+      });
+    },
     setActive() {
       this.active = true
       this.$emit('changeActive', this);
+    },
+    async activeNotifications() {
+      await Notification.requestPermission();
+      this.hasNotification = Notification.permission === "granted"
     }
   },
   watch: {
-    'active': function() {
-    }
   },
   template: '#pomodoro-template'
 }
